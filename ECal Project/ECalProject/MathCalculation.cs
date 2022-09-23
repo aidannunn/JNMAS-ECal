d2697e971f53a5e0c59c0ac0cd19688810f36b4d
Musa-Husseini <74230559+Musa-Husseini@users.noreply.github.com>
2022-09-22 18:25:40 -0700

    Added new class to calculated advanced computation

---
+++ ECal Project/ECalProject/MathCalculation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECalProject
+{
+    internal class MathCalculation
+    {
+
+        //Square root
+        public double squareRoot(double num)
+        {
+            return Math.Sqrt(num);
+        }
+
+        //power
+        public double power(double num, double power)
+        {
+            return Math.Pow(num, power);
+        }
+
+        //Natural log
+        public double ln(double num)
+        {
+            return Math.Log(num);
+        }
+
+        //Log
+        public double log(double num)
+        {
+            return Math.Log10(num);
+        }
+
+        //Sine
+        public double sine(double num)
+        {
+            return Math.Sin(num);
+        }
+
+        //Cosine
+        public double cosine(double num)
+        {
+            return Math.Cos(num);
+        }
+
+        //Tangent
+        public double tangent(double num)
+        {
+            return Math.Tan(num);
+        }
+
+        //Radian
+        public double radian(double num)
+        {
+            return (Math.PI / 180) * num;
+        }
+
+        //Factorial
+        public double factorial(double num)
+        {
+            double ans = 1;
+
+            while(num != 0)
+            {
+                ans *= num;
+                num -= 1;
+            }
+
+            return ans;
+        }
+
+        //Percent
+        public double percent(double num1, double num2)
+        {
+            double prod = num1 * num1;
+            return prod / 100;
+        }
+
+        //e-constant (euler's constant)
+        public double eConstant(double num)
+        {
+            return Math.Pow(Math.E, num);
+        }
+
+        //Inverse function
+        public double inverseFunction(double num)
+        {
+            return 1 / num;
+        }
+    }
+}
