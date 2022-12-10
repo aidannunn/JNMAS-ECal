efbc998c76377853612981cf43c9be0f02bbc7cc
Musa-Husseini <74230559+Musa-Husseini@users.noreply.github.com>
2022-12-09 20:22:00 -0800

    Added BVA tests

---
+++ ECal Project/Musa's Integration Tests/BlackBoxTests.cs
@@ -0,0 +1,86 @@
+﻿using Moq;
+using NUnit.Framework;
+using SpreadsheetEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musa_s_Integration_Tests
+{
+    public class BlackBoxTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void testBVAGetPrecedence()
+        {
+            var mock = new Mock<OperatorNodeFactory_IntegrationTesting>();
+            mock.CallBase = true;
+
+            //Assert GetPresedence
+            OperatorNodeFactory_IntegrationTesting mockOperator = mock.Object;
+
+
+            int min = int.MinValue;
+            int minUp = int.MinValue + 1;
+            int nom = 1;
+            int maxDown = int.MaxValue - 1;
+            int max = int.MaxValue;
+            Assert.AreNotEqual(mockOperator.GetPrecedence('+'), min);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('+'), minUp);
+            Assert.AreEqual(mockOperator.GetPrecedence('+'), nom);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('+'), maxDown);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('+'), max);
+
+
+            Assert.AreNotEqual(mockOperator.GetPrecedence('-'), min);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('-'), minUp);
+            Assert.AreEqual(mockOperator.GetPrecedence('-'), nom);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('-'), maxDown);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('-'), max);
+
+
+            min = int.MinValue;
+            minUp = int.MinValue + 1;
+            nom = 2;
+            maxDown = int.MaxValue - 1;
+            max = int.MaxValue;
+
+            Assert.AreNotEqual(mockOperator.GetPrecedence('*'), min);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('*'), minUp);
+            Assert.AreEqual(mockOperator.GetPrecedence('*'), nom);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('*'), maxDown);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('*'), max);
+
+            Assert.AreNotEqual(mockOperator.GetPrecedence('/'), min);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('/'), minUp);
+            Assert.AreEqual(mockOperator.GetPrecedence('/'), nom);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('/'), maxDown);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('/'), max);
+
+
+
+            min = int.MinValue;
+            minUp = int.MinValue + 1;
+            nom = 3;
+            maxDown = int.MaxValue - 1;
+            max = int.MaxValue;
+
+            Assert.AreNotEqual(mockOperator.GetPrecedence('^'), min);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('^'), minUp);
+            Assert.AreEqual(mockOperator.GetPrecedence('^'), nom);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('^'), maxDown);
+            Assert.AreNotEqual(mockOperator.GetPrecedence('^'), max);
+            Assert.That(mockOperator.GetPrecedence('^'), Is.EqualTo(3));
+
+
+        }
+
+    }
+}
